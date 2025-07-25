7767b4750638ed60411bd65155e3d4aae82c4fb1
Ayobamidele Oluwasetemi <ayobamidelesetemi@gmail.com>
2025-07-25 11:47:11 +0100

    Refactor code structure for improved readability and maintainability

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
   API_BASE_URL: "https://core.hisend.hunnovate.com/api/v1/projects",
-  API_KEY: 'QEaY0hOYpj1SZ3hj9dUirgki',
+  API_KEY: '7cLG41V6vQV19gDy9w2ERhRx',
   PROJECT_ID: 'shop_cart',
 };
